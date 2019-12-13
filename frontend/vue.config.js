534fbb866622c10d37a925db56f9dafe46910808
Nguyễn Hải Quang <haiquang9994@gmail.com>
2019-12-13 10:58:22 +0700

    code

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,8 +4,6 @@ module.exports = {
     publicPath: '/admin',
     outputDir: '../public/admin',
     devServer: {
-        host: 'framework.lc',
-        allowedHosts: ['framework.lc'],
         port: 7979
     }
 }
